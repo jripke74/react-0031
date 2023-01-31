7beec80b856d9886250e45a0c2dac955739a6f6d
Jeff Ripke <jripke74@gmail.com>
2023-01-31 11:12:34 -0800

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { Fragment, useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -12,7 +13,9 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        'https://react-http-4da68-default-rtdb.firebaseio.com/movies.json'
+      );
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
@@ -36,7 +39,11 @@ function App() {
 
   useEffect(() => {
     fetchMoviesHandler();
-  }, []);
+  }, [fetchMoviesHandler]);
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
   let content = <p>Found no movies.</p>;
 
@@ -53,12 +60,15 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>{content}</section>
-    </Fragment>
+    </React.Fragment>
   );
 }
 
